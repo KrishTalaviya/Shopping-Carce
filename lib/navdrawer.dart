aa79fe3db314bbcefa8d7da43e07c2bf18428826
KrishTalaviya <krishtalaviya987@gmail.com>
2023-02-28 10:42:58 +0530

    Shopping app Crace

---
+++ lib/navdrawer.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:shopingapp/demo.dart';
+import 'package:shopingapp/home.dart';
+
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+            ),
+            child: Center(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                    flex: 2,
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "Carce",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text("Home"),
+            leading: IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (BuildContext context) => Home()));
+            },
+          ),
+          Divider(
+            color: Colors.grey,
+          ),
+          ListTile(
+            title: Text("Shop"),
+            leading: IconButton(
+              icon: Icon(Icons.shop),
+              onPressed: () {},
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (BuildContext context) => Demo()));
+            },
+          ),
+          Divider(
+            color: Colors.grey,
+          ),
+          ListTile(
+            title: Text("Profile"),
+            leading: IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {},
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (BuildContext context) => Demo()));
+            },
+          ),
+          Divider(
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
